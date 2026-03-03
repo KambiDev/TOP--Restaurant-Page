9e663e3dc546c98c8361ed270b2a0cecfa79403e
kambises Elias <kambiseselias89@gmail.com>
2026-03-03 01:08:53 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./css/styles.css";
-import { createHeader } from "./js/header.js";
-import { createMain } from "./js/main.js";
-import { createFooter } from "./js/footer.js";
+import './css/styles.css';
+import { createHeader } from './js/header.js';
+import { createMain } from './js/main.js';
+import { createFooter } from './js/footer.js';
 
-const content = document.querySelector("#content");
+const content = document.querySelector('#content');
 const myHeader = createHeader();
 const myMain = createMain();
 const myFooter = createFooter();
